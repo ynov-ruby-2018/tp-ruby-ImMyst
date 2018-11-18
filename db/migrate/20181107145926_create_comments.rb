a3261f4738b008a4cbd8fc006f408d67ea28632e
Guillaume Barillot <gbarillot@gmail.com>
2018-11-18 17:28:02 +0100

    Update 20181107145926_create_comments.rb

--- db/migrate/20181107145926_create_comments.rb
+++ db/migrate/20181107145926_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
     create_table :comments do |t|
       t.timestamps
       t.text :content
+      # Manque user_id pour la relation user <-> comments
    end
  end
 end
