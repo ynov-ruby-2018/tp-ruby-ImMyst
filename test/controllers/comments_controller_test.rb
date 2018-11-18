b1ae272893e737df41141f00b558ca5028d561f1
Guillaume Barillot <gbarillot@gmail.com>
2018-11-18 17:29:50 +0100

    Update comments_controller_test.rb

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,12 +1,14 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
+  # BIEN !
   test "should get index" do
     get comments_index_url
     assert_response :success
   end
 
   test "should get create" do
+    # Ok, ça marche, mais vous testez #index, pas le #create !
     get comments_create_url
     assert_response :success
   end
